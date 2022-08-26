b83513d52179119a05b4735ae41d61ffc322a70c
JustKaneri <justkaneri@yandex.ru>
2022-08-26 19:59:26 +0300

    контролер и представления модели Категория

---
+++ Back/FirtApp/FistAppMVC/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using FistAppMVC.Data;
+using FistAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FistAppMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+
+            return View(objCategoryList);
+        }
+    }
+}
