14025583e80f1de86e7f8c69d81666436884b206
JustKaneri <justkaneri@yandex.ru>
2022-09-01 21:56:59 +0300

    Редактирование и удаление

--- Back/FirtApp/FistAppMVC/Controllers/CategoryController.cs
+++ Back/FirtApp/FistAppMVC/Controllers/CategoryController.cs
@@ -45,5 +45,69 @@ namespace FistAppMVC.Controllers
             return View(obj);
             
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id==0)
+            {
+                return NotFound();
+            }
+
+            Category ctg = _db.Categories.Find(id);
+
+            if(ctg == null)
+            {
+                return NotFound();
+            }
+
+            return View(ctg);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category ctg = _db.Categories.Find(id);
+
+            if (ctg == null)
+            {
+                return NotFound();
+            }
+
+            return View(ctg);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
