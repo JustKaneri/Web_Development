4e331d789b741b49dd3162e4bc44c00f4327b8fe
JustKaneri <justkaneri@yandex.ru>
2022-09-08 20:35:44 +0300

    сортировка

--- Back/first-app/src/App.js
+++ Back/first-app/src/App.js
@@ -1,14 +1,17 @@
 import React, { useRef, useState } from 'react';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
+import MySelect from './components/UI/select/MySelect';
 
 import './styles/App.css';
 
 function App() {
-  const [posts,setPosts] = useState([{id: 1, title:"JS",body: "discription" },
-                                    {id: 2, title:"C#",body: "discription" },
-                                    {id: 3, title:"C++",body: "discription" },
-                                    {id: 4, title:"Rybu",body: "discription" }]);
+    const [posts,setPosts] = useState([{id: 1, title:"JS",body: "discription" },
+                                    {id: 2, title:"C#",body: "fiscription" },
+                                    {id: 3, title:"C++",body: "ziscription" },
+                                    {id: 4, title:"Rybu",body: "qiscription" }]);
+    
+    const [selectedSort,setSeleceteSort] = useState();
 
     const createPost = (newPost) =>{
         setPosts([...posts,newPost]);
@@ -18,12 +21,28 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     }
 
+  const sortPosts = (sort) =>{
+      setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])));
+  }
+
   return (
     <div className="App">
         <PostForm create={createPost}/>
+        <hr style={{margin:'20px'}}/>
+        <div>
+            <MySelect
+              onChange={sortPosts}
+              value={selectedSort}
+              options={[ 
+                {value:'title', name:'По названию'},
+                {value:'body', name:'По описанию'}
+              ]}
+              defaultValue="Сортировка"
+            />
+        </div>
         {posts.length !== 0
             ? <PostList remove={removePost} posts={posts}  title= "Список постов 1"/>   
-            : <div>Посты не найдены</div>
+            : <h1 style={{textAlign:'center'}}>Записей нет</h1>
         }
        
     </div>
