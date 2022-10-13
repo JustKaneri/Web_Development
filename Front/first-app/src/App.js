782fb434f2b1e5af81675f710276c26c0187db44
JustKaneri <justkaneri@yandex.ru>
2022-10-13 23:28:21 +0300

    React улучешение навигации

--- Front/first-app/src/App.js
+++ Front/first-app/src/App.js
@@ -4,19 +4,34 @@ import Navigation from './components/Navigation';
 import About from "./pages/About";
 import PostIdPage from './pages/PostIdPage';
 import Posts from './pages/Posts';
+import { privatRoutes, publicRoutes } from './router/routes';
 import "./styles/App.css";
 
 
 function App() {
+
+  const isAuth = false;
+
   return (
     <Router>
       <Navigation/>
-      <Routes>
-        <Route path='/about' element={<About/>}/>
-        <Route exact path='/posts' element={<Posts/>}/>
-        <Route exact path='/posts/:id' element={<PostIdPage/>}/>
-        <Route path='*' element={<Navigate to={"/posts"}/>}/>
-      </Routes>
+      {isAuth
+        ?  <Routes>
+              {publicRoutes.map((obj)=>
+                <Route exact = {obj.exact} path={obj.path} element={obj.element}/>
+              )}
+              {privatRoutes.map((obj)=>
+                <Route exact = {obj.exact} path={obj.path} element={obj.element}/>
+              )}
+              <Route path='*' element={<Navigate to={"/login"}/>}/>
+            </Routes>
+        : <Routes>
+              {publicRoutes.map((obj)=>
+                <Route exact = {obj.exact} path={obj.path} element={obj.element}/>
+              )}
+              <Route path='*' element={<Navigate to={"/login"}/>}/>
+          </Routes>
+      }
     </Router>
   )
 }
