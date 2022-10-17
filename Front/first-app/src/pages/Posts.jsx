3b91aec06ab69b74a6a0560e1d407681e81c554b
JustKaneri <justkaneri@yandex.ru>
2022-10-17 20:56:28 +0300

    React бесконечная лента

--- Front/first-app/src/pages/Posts.jsx
+++ Front/first-app/src/pages/Posts.jsx
@@ -24,13 +24,27 @@ function Posts() {
   const [page,setPage] = useState(1);
   const sortedAndSearchPosts = usePosts(posts,filter.sort,filter.query);
   const [fetchPosts,isPostLoading,postError] = useFetching(async () =>{
-    const posts = await PostsSevice.getAll(limit,page);
-    setPosts(posts.data);
-    const totalCount = posts.headers["x-total-count"];
+    const postsD = await PostsSevice.getAll(limit,page);
+    setPosts([...posts,...postsD.data]);
+    const totalCount = postsD.headers["x-total-count"];
     setTotalPage(getPageCount(totalCount,limit));
   });
+  const lastElement = useRef();
+  const observer = useRef();
 
-  
+  useEffect(()=>{
+    if(isPostLoading) return;
+    if(observer.current) observer.current.disconnect();
+    var callback = function(entries,observer){
+      if(entries[0].isIntersecting && page < totalPage){
+        console.log(page);
+        setPage(page+1);
+      }
+    }
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current);
+
+  },[isPostLoading]);
 
   useEffect(()=>{
     fetchPosts();
@@ -63,10 +77,12 @@ function Posts() {
       {postError && 
         <h1>Произошла ошибка {postError}</h1>
       }
-      {isPostLoading 
-          ?<div style={{display: "flex", justifyContent:'center'}}><Loader/></div>
-          :<PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов 1" />
+      <PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов 1" />
+      <div ref = {lastElement} style = {{height:'10px'}}></div>
+      {isPostLoading &&
+        <div style={{display: "flex", justifyContent:'center'}}><Loader/></div>
       }
+      
       <br></br>
       <Pagination
       totalPage={totalPage}
